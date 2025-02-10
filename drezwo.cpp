a8fbc45c92fe36fd403b7fd52548dc7209ec7607
mania2804 <dziedzicmaria2@gmail.com>
2025-02-10 20:27:23 +0000

    drezwo

---
+++ drezwo.cpp
@@ -0,0 +1,1 @@
+binarne ma tylko 2 dzieci na 1 rodzica
--- wczytywanie pliku.cpp
+++ wczytywanie pliku.cpp
@@ -1,4 +1,8 @@
-ifstream file("slownik_11.txt" (mode) ios::binary ; // Otwieramy plik ze słownikiem
-    if (!file.is_open()) {
-        cerr << "Nie można otworzyć pliku!" << endl;
-        return -1;
+PGM read_PGM() {
+    ifstream file("lokomotywa.pgm", ios::binary);
+    if (!file.is_open())
+        throw runtime_error("Nie można otworzyć pliku.");
+PGM
+    file >> image.format;
+    file >> image.width >> image.height;
+    file >> image.max_gray;
